dfa31f80e6b5d3f2655b583241c7dde710e12686
David <dadastudios382@gmail.com>
2023-02-23 14:47:57 -0500

    fixing UI bugs in landing page

--- src/pages/Home/components/QuestionExample.tsx
+++ src/pages/Home/components/QuestionExample.tsx
@@ -1,7 +1,9 @@
 import { useId, useState } from "react"
+import { toast } from "react-hot-toast"
 import MarkdownBody from "../../../components/ui/MarkdownBody"
 import Separator from "../../../components/ui/Separator"
 import { IQuestion, ISBAQuestion } from "../../../interface/exam"
+import { Link } from "react-router-dom"
 
 const QuestionExample = () => {
 	const id = useId()
@@ -70,6 +72,7 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 
 	const [hasAnswered, setHasAnswered] = useState(false)
 	const [showModal, setShowModal] = useState(true)
+	const [selectedOption, setSelectedOption] = useState("")
 
 	function restartState() {
 		setHasAnswered(false)
@@ -80,35 +83,47 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 	}
 
 	function simulateSubmitting() {
+		if (!selectedOption) return toast.error("Select an option before")
+
+		if (
+			selectedOption ===
+			question.content.options[question.content.answer - 1]
+		)
+			toast.success("Correct answer")
+		else toast.error("Incorrect answer... Try again")
 		setHasAnswered(true)
 	}
 
 	return (
 		<div
-			className={`relative bg-slate-800 max-h-[600px] p-8 rounded-md shadow-md border border-gray-100/10 text-white ${
-				showModal ? "overflow-y-" : "overflow-y-auto"
-			}`}>
+			className={`bg-slate-800 max-h-[600px] rounded-md relative shadow-md border border-gray-100/10 text-white ${
+				showModal ? "overflow-y-hidden" : "overflow-y-auto"
+			}`}
+		>
 			<div
-				className={`absolute inset-x-0 duration-700 transition-all bg-opacity-100 bg-gradient-to-b from-slate-900/50 to-black/70 flex items-center justify-center bottom-0 ${
+				className={`absolute inset-x-0 duration-700 transition-all bg-opacity-100 bg-gradient-to-b from-slate-900/50 to-black/70 flex items-center justify-center -bottom-3 ${
 					showModal
 						? "top-0 opacity-100 pointer-events-auto"
 						: "top-full opacity-0 pointer-events-none"
-				}`}>
+				}`}
+			>
 				<button
 					onClick={startSimulation}
-					className={`flex flex-col gap-4 items-center text-center text-5xl z-10 font-bold text-gray-300 p-5 relative after:absolute after:hover:bg-slate-900/80 after:transition-all after:duration-500 after:left-1/2 after:right-1/2 after:top-0 after:bottom-0 after:hover:left-0 after:hover:right-0 after:-z-10 after:rounded-md after:shadow-xl`}>
+					className={`flex flex-col gap-4 items-center text-center text-5xl z-10 font-bold text-gray-300 p-5 relative after:absolute after:hover:bg-slate-900/80 after:transition-all after:duration-500 after:left-1/2 after:right-1/2 after:top-0 after:bottom-0 after:hover:left-0 after:hover:right-0 after:-z-10 after:rounded-md after:shadow-xl`}
+				>
 					<h3>Give it a try!</h3>
 					<svg
 						className='w-12'
 						fill='currentColor'
 						viewBox='0 0 20 20'
 						xmlns='http://www.w3.org/2000/svg'
-						aria-hidden='true'>
+						aria-hidden='true'
+					>
 						<path d='M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z' />
 					</svg>
 				</button>
 			</div>
-			<div className=' flex flex-col gap-3 text-gray-200 font-medium'>
+			<div className='flex flex-col gap-3 text-gray-200 font-medium p-8'>
 				<span className='text-sm text-gray-300'>
 					Category - <b>{question.category}</b>
 				</span>
@@ -118,7 +133,8 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 						fill='currentColor'
 						viewBox='0 0 20 20'
 						xmlns='http://www.w3.org/2000/svg'
-						aria-hidden='true'>
+						aria-hidden='true'
+					>
 						<path
 							clipRule='evenodd'
 							fillRule='evenodd'
@@ -134,12 +150,13 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 				<Separator />
 				<MarkdownBody content={question.scenario} />
 				<p className='text-gray-300'>{question.content.question}</p>
-				<form>
+				<div>
 					<ol type='A' className='inline-flex flex-col mt-2 mb-1'>
 						{question.content.options.map((option, index) => (
 							<li
 								className='list-[upper-latin] list-inside first:rounded-t-md last:rounded-b-md border border-gray-100/10 py-2 px-4'
-								key={option + index}>
+								key={option + index}
+							>
 								<label
 									className={`${
 										hasAnswered
@@ -149,11 +166,15 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 												: "text-red-500"
 											: "text-gray-300"
 									}`}
-									htmlFor={option + index}>
+									htmlFor={option + index}
+								>
 									{option}
 									<input
 										className={`ml-4`}
 										type='radio'
+										onChange={e =>
+											setSelectedOption(e.target.value)
+										}
 										value={option}
 										id={option + index}
 										name='optionSelected'
@@ -166,19 +187,21 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 						<button
 							onClick={simulateSubmitting}
 							type='button'
-							className='flex items-center gap-2 py-2 px-3 border border-blue-500/50 hover:bg-blue-700/50 bg-blue-800/50 rounded-md mt-6 mb-1'>
+							className='flex items-center gap-2 py-2 px-3 border border-blue-500/50 hover:bg-blue-700/50 bg-blue-800/50 rounded-md mt-6 mb-1'
+						>
 							<svg
 								className='w-5'
 								fill='currentColor'
 								viewBox='0 0 20 20'
 								xmlns='http://www.w3.org/2000/svg'
-								aria-hidden='true'>
+								aria-hidden='true'
+							>
 								<path d='M3.105 2.289a.75.75 0 00-.826.95l1.414 4.925A1.5 1.5 0 005.135 9.25h6.115a.75.75 0 010 1.5H5.135a1.5 1.5 0 00-1.442 1.086l-1.414 4.926a.75.75 0 00.826.95 28.896 28.896 0 0015.293-7.154.75.75 0 000-1.115A28.897 28.897 0 003.105 2.289z' />
 							</svg>
 							Submit answer
 						</button>
 					)}
-				</form>
+				</div>
 				{hasAnswered && (
 					<>
 						<MarkdownBody content={question.content.explanation} />
@@ -186,13 +209,15 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 							<button
 								onClick={restartState}
 								type='button'
-								className='self-end flex items-center gap-2 py-2 px-3 border border-amber-500/50 hover:bg-amber-700/50 bg-amber-800/50 rounded-md mt-6 mb-1'>
+								className='self-end flex items-center gap-2 py-2 px-3 border border-amber-500/50 hover:bg-amber-700/50 bg-amber-800/50 rounded-md mt-6 mb-1'
+							>
 								<svg
 									className='w-5'
 									fill='currentColor'
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'
-									aria-hidden='true'>
+									aria-hidden='true'
+								>
 									<path
 										clipRule='evenodd'
 										fillRule='evenodd'
@@ -201,23 +226,25 @@ Physical examination reveals subcostal recessions, nasal flaring and wheezing. T
 								</svg>
 								Try Again
 							</button>
-							<button
+							<Link to="/subscription"
 								type='button'
-								className='self-end flex items-center gap-2 py-2 px-3 border border-blue-500/50 hover:bg-blue-700/50 bg-blue-800/50 rounded-md mt-6 mb-1'>
+								className='self-end flex items-center gap-2 py-2 px-3 border border-blue-500/50 hover:bg-blue-700/50 bg-blue-800/50 rounded-md mt-6 mb-1'
+							>
 								<svg
 									className='w-5'
 									fill='currentColor'
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'
-									aria-hidden='true'>
+									aria-hidden='true'
+								>
 									<path
 										clipRule='evenodd'
 										fillRule='evenodd'
 										d='M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z'
 									/>
 								</svg>
-								Next
-							</button>
+								Keep Excel-ing
+							</Link>
 						</div>
 					</>
 				)}
