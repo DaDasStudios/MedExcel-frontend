2cc1a9ece03a311e443d61a3b27a3c31f8fdbc8e
DaDasStudios <dadastudios382@gmail.com>
2023-07-14 10:54:53 -0500

    fix(stats): add loading state to `CategoryStats`
    Avoid the component get stuck loading when
    user doesn't have questions to analyze

--- src/pages/Account/components/CategoryStats.tsx
+++ src/pages/Account/components/CategoryStats.tsx
@@ -17,31 +17,29 @@ const CategoryStats = () => {
 	const { auth } = useAuthContext()
 
 	const [stats, setStats] = useState(null as IChartStats[] | null)
-    const [bestCategory, setBestCategory] = useState("")
+	const [loading, setLoading] = useState(true)
+	const [bestCategory, setBestCategory] = useState("")
 	const [worstCategory, setWorstCategory] = useState("")
 
 	async function getStats() {
 		try {
-			const res = await getGeneralPerformanceRequest(
-				auth.id || "",
-				auth.token || ""
-			)
+			setLoading(true)
+			const res = await getGeneralPerformanceRequest(auth.id || "", auth.token || "")
 
 			if (res.data.status === "CORRECT") {
-				const payload: IChartStats[] = Object.entries(
-					res.data.statistics.categoriesPerformance
-				).map(stat => {
-					return {
-						name: toTitle(stat[0]),
-						count: stat[1].count,
+				const payload: IChartStats[] = Object.entries(res.data.statistics.categoriesPerformance).map(
+					([name, value]) => {
+						return {
+							name: toTitle(name),
+							count: value.count,
+						}
 					}
-				})
+				)
 				setStats(payload)
-                setBestCategory(res.data.statistics.bestCategory)
-                setWorstCategory(res.data.statistics.worstCategory)
+				setBestCategory(res.data.statistics.bestCategory)
+				setWorstCategory(res.data.statistics.worstCategory)
 			}
 		} catch (error: any) {
-			console.log(error)
 			switch (error.response.status) {
 				case 500:
 					toast.error("Something went wrong during stats calculation")
@@ -53,74 +51,57 @@ const CategoryStats = () => {
 				default:
 					break
 			}
+		} finally {
+			setLoading(false)
 		}
 	}
 
 	useEffect(() => {
-		if (auth.user?.subscription?.hasSubscription) {
-			setTimeout(getStats, 1000)
-		} else {
-			setStats([])
-		}
+		getStats()
 	}, [auth.user?.subscription])
+
+	if (loading) {
+		return (
+			<div className='flex items-center justify-center'>
+				<Spin />
+			</div>
+		)
+	}
 
 	return (
 		<div>
-			{stats === null ? (
-				<div className='flex items-center justify-center'>
-					<Spin />
-				</div>
-			) : (
+			{stats && stats.length > 0 ? (
 				<>
-					<h3 className='text-base text-slate-300 font-medium mb-6'>
-						Performance in categories
-					</h3>
-					{stats.length > 0 ? (
-						<>
-							<BarChart
-								className='max-md:hidden'
-								width={450}
-								height={300}
-								data={stats}
-							>
-								<CartesianGrid
-									strokeDasharray={10}
-									stroke={neutralChartTheme.backgroundColor}
-								/>
-								<YAxis stroke={neutralChartTheme.borderColor} />
-								<XAxis
-									dataKey='name'
-									stroke={neutralChartTheme.borderColor}
-									tickCount={4}
-									fontSize={14}
-								/>
-								<Bar
-									dataKey='count'
-									fill={neutralChartTheme.backgroundColor}
-									stroke={neutralChartTheme.borderColor}
-								/>
-							</BarChart>
-							<div className='grid grid-cols-1 sm:grid-cols-2 gap-x-5'>
-								<Input
-									id='bestCategory'
-									label='Best Category'
-									name='bestCategory'
-									value={toTitle(bestCategory)}
-								/>
-								<Input
-									id='worstCategory'
-									label='Worst Category'
-									name='worstCategory'
-									value={toTitle(worstCategory)}
-								/>
-							</div>
-						</>
-					) : (
-						<p className='font-medium text-2xl text-center text-slate-300 underline mb-4'>
-							Don't have questions to analyze
-						</p>
-					)}
+					<h3 className='text-base text-slate-300 font-medium mb-6'>Performance in categories</h3>
+					<BarChart className='max-md:hidden' width={450} height={300} data={stats}>
+						<CartesianGrid strokeDasharray={10} stroke={neutralChartTheme.backgroundColor} />
+						<YAxis stroke={neutralChartTheme.borderColor} />
+						<XAxis dataKey='name' stroke={neutralChartTheme.borderColor} tickCount={4} fontSize={14} />
+						<Bar
+							dataKey='count'
+							fill={neutralChartTheme.backgroundColor}
+							stroke={neutralChartTheme.borderColor}
+						/>
+					</BarChart>
+					<div className='grid grid-cols-1 sm:grid-cols-2 gap-x-5'>
+						<Input
+							id='bestCategory'
+							label='Best Category'
+							name='bestCategory'
+							value={toTitle(bestCategory)}
+						/>
+						<Input
+							id='worstCategory'
+							label='Worst Category'
+							name='worstCategory'
+							value={toTitle(worstCategory)}
+						/>
+					</div>
 				</>
+			) : (
+				<p className='font-medium text-2xl text-center text-slate-300 underline mb-4'>
+					Don't have questions to analyze
+				</p>
 			)}
 		</div>
 	)
